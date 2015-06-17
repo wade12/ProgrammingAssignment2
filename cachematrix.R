b88e2f575ce8640504c52a4799f594f44987fe98
wade12 <wadetaylor@engineer.com>
2015-06-17 14:59:32 +0100

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,40 +3,55 @@
 ## These 2 functions cache the inverse of a matrix.
 
 ## makeCacheMatrix:
-## This function creates a special "matrix" object that can cache its inverse
+## This function creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
+makeCacheMatrix <- function( x = matrix() ) {
+        
+        invert <- NULL
+        
         set <- function(y) {
                 x <<- y
-                m <<- NULL
-        }
+                invert <<- NULL
+        } ## end set
+        
         get <- function() x
-        setmean <- function(mean) m <<- mean
-        getmean <- function() m
-        list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
+        
+        setinverse <- function(inverse) invert <<- inverse
+        getinverse <- function() invert
+        
+        list( set = set, get = get, setinverse = setinverse, getinverse = getinverse )
         
 } ## end function makeCacheMatrix
 
 
+
 ## cacheSolve:
-## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
 ## If the inverse has already been calculated (and the matrix has not changed),
 ## then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         
-        m <- x$getmean()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
-        data <- x$get()
-        m <- mean(data, ...)
-        x$setmean(m)
-        m
+        invert <- x$getinverse()
         
-} ## endn function cacheSolve
+        if(!is.null(inv)) {
+                message("retrieving cached data")
+                return(invert)
+        } ## end if
+        
+        data <- x$get()
+        invertt <- Solve(Data)
+        x$setinverse(invert)
+        
+        invert
+        
+} ## end function cacheSolve
+
+
+## sample matrix (Fibonacci Sequence)
+X = matrix(c(0, 1, 2, 3, 5, 8, 13, 21, 34), nrow = 3, ncol = 3)
+X ## print matrix
+M = makeCacheMatrix(x) ## run function
+M ## print inverse matrix
+## M$get()
